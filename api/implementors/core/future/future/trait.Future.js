5e000f6c8e7fc0dca29fef1b9854e7506dc0fcb2
Deploy from CI <>
2021-03-08 21:03:15 +0000

    Deploy f8cc824396691bceafae55cadae08ff294ae15de to gh-pages

---
+++ api/implementors/core/future/future/trait.Future.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["wasmtime"] = [{"text":"impl&lt;R&gt; Future for WasmCall&lt;R&gt;","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
